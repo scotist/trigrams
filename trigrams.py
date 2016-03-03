ba1036692ff405d34b797c6178f94d1c3c96980d
Michael Sullivan <sapientia.veritas@gmail.com>
2016-03-03 13:40:53 -0800

    inserted Munir's functions, not totally working yet

--- trigrams.py
+++ trigrams.py
@@ -1,6 +1,7 @@
 """Generate random text based on model text."""
 import io
 import string
+import random
 
 
 def read_file(path):
@@ -12,35 +13,47 @@ def read_file(path):
 
 
 def tokenize(data):
-    """Split out sample text and remove punctuation."""
+    """Split out sample text and removes punctuation."""
     for char in string.punctuation:
         data = data.replace(char, "")
     data_list = data.split()
+    tuplize(data_list)
     return data_list
 
 
-def dictionize(list):
-    """Put our text in a dict."""
-    trigrams = {}
-    while len(list) >= 3:
-        first_key = list.pop(0)
-        second_key = list[0]
-        value = list[1]
-        if (first_key, second_key) in trigrams.keys():
-            trigrams[(first_key, second_key)].append(value)
-        else:
-            trigrams[(first_key, second_key)] = [value]
-    print(trigrams)
-    return trigrams
+def tuplize(d_list):
+    """Convert list to a list of tuples."""
+    dictionary = {}
+    for i in range(len(d_list) - 2):
+        dict_keys = (d_list[i], d_list[i + 1])
+        dictionary.setdefault(dict_keys, []).append(d_list[i + 2])
+    first_two_words(dictionary)
+    return dictionary
 
 
-def main():
-    """Put it all together."""
-    pass
+def first_two_words(dict):
+    """Create a random paragraph using the dictionary."""
+    random_key = random.choice(list(dict.keys()))
+    start_paragraph = [' '.join((random_key[0], random_key[1]))]
+    create_paragraph(random_key, start_paragraph, dict)
+    return start_paragraph
+
+
+def create_paragraph(keys, para, dict):
+    """Create the whole paragraph."""
+    # print(para)
+    random_word = (keys[0], keys[1])
+    while dict.get(random_word, None):
+        if para[-1] is None:
+            break
+        para.append(random.choice(dict[random_word]))
+        random_word = (para[-2], para[-1])
+        # continue
+    print(para)
 
 
 if __name__ == '__main__':
     pass
 
 
-dictionize(read_file("sample.txt"))
+read_file("sample.txt")
