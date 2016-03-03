0a5c201b39eec25d46e540bb935f37a3db77a415
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-03-02 20:35:33 -0800

    create the dict of tuples and lists

--- trigrams.py
+++ trigrams.py
@@ -11,21 +11,26 @@ def read_file(path):
 
 
 def tokenize(data):
-	"""Splits out sample text and removes punctuation."""
+    """Split out sample text and removes punctuation."""
     for char in string.punctuation:
         data = data.replace(char, "")
     data_list = data.split()
-    print(data_list)
+    tuplize(data_list)
     return data_list
 
 
-def tuplize():
-	"""Convert list to a list of tuples."""
-
-
-def dictionize():
-    """Put our text in a dict."""
-
+def tuplize(d_list):
+    """Convert list to a list of tuples."""
+    d = {}
+    for i in range(len(d_list) - 2):
+        tup = (d_list[i], d_list[i + 1])
+        d.setdefault(tup, []).append(d_list[i + 2])
+        # if d[tup]:
+        #     d[tup].append(d_list[i + 2])
+        # else:
+        #     d[tup] = [d_list[i + 2]]
+    print(d)
+    return d
 
 if __name__ == '__main__':
     pass
